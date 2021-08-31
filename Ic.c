6a0bc65aeb0d417bd1ff9b321b2a4511ae2522db
mmdbalkhi <ahmdparsh129@gmail.com>
2021-08-31 11:05:42 +0430

    add document to Ic.c

--- Ic.c
+++ Ic.c
@@ -1,3 +1,13 @@
+/*
+* Ic is a repl for C and tries to make something like Ipython *
+* but Ic is not a copy of Ipython but something like that *
+* and [probably] its graphical environment is used in Ic *
+************************************************************
+****************GNU GENERAL PUBLIC LICENSE******************
+**************** Version 3, 29 June 2007 *******************
+************************************************************
+*/
+
 #include <stdio.h>
 
 int main()
